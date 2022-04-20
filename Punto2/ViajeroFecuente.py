296615f41434b25c5aa37064f91aaae1895b88b6
MoraJuan <73558008+MoraJuan@users.noreply.github.com>
2022-04-19 22:56:28 -0300

    Add files via upload

---
+++ Punto2/ViajeroFecuente.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 15 19:25:45 2022
+
+@author: Juan
+"""
+
+ class Viajero:
+     __numviajero = ''
+     __dni = ''
+     __nombre = ''
+     __apellido = ''
+     __millas_acum = ''
+     
+     def __init__(self, numviejaro= '', dni= '', nombre='', apellido='', millas_acum=''):
+         self.__numviajero = num_viajero
+         self.__dni = dni
+         self.__nombre= nombre
+         self.__apellido= apellido
+         self.__millas_acum= millas_acum
+        
+    def cantidadTotaldeMillas(self)
+        
