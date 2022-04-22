7044c492d7746932573ff0da24b094cb2a7feb54
MoraJuan <73558008+MoraJuan@users.noreply.github.com>
2022-04-22 18:39:57 -0300

    Add files via upload

--- Ejercicio2-main/Punto2/ViajeroFecuente.py
+++ Ejercicio2-main/Punto2/ViajeroFecuente.py
@@ -14,10 +14,25 @@ class Viajero:
          self.__millas = millas
          
     def __str__(self):
-        return str(self.__numviajero)
+        return str(self.__numviajero, self.__millas)
+    
+
+    def getNumero(self):
+        return self.__numviajero
                 
    
     def cantidadTotaldeMillas(self):
         return 'Cantidad de millas realizadas \n' + str(self.__millas)
     
-    #def Acummillas(self):        
+    def Acummillas(self, acum_millas):
+        
+        acum = self.__millas + acum_millas
+        self.__millas=acum
+        return (self.__millas)
+    def canjearMillas(self, millas_canjear):
+        if millas_canjear <= self.__millas:
+            self.__millas = self.__millas - millas_canjear 
+        else:
+            print('No es posible el cambio las millas a canjear son mayores a las millas totales')
+        return self.__millas
+        
